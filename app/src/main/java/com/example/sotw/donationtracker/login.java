28e3a7d50d8ff8509cc8e568caf67bb8506bb25d
Cscherban <christopher.scherban@gmail.com>
2018-09-23 19:59:55 -0400

    added firebase login

--- app/src/main/java/com/example/sotw/donationtracker/login.java
+++ app/src/main/java/com/example/sotw/donationtracker/login.java
@@ -3,18 +3,68 @@ package com.example.sotw.donationtracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.*;
+
+import android.support.annotation.NonNull;
+
 public class login extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+
+    /**
+     * This is what success looks like:
+     * @param user - the user object returned by firebase
+     */
+    private void success(FirebaseUser user){
+        //Do Stuff with the firebase user
+
+        Intent nextScreen = new Intent(getApplicationContext(), Account.class);
+        startActivity(nextScreen);
+
+    }
+
+    /**
+     * Function to handle login failure
+     */
+    private void failure(){
+
+        TextView status = (TextView) findViewById(R.id.status);
+        status.setText("Incorrect username or password");
+
+    }
+
+    private void attemptLogin(String email, String pass){
+        mAuth.signInWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Success Message", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            success(user);
+                        }else {
+                            failure();
+                        }
+
+                    }
+                });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
 
         Button loginButton = (Button) findViewById(R.id.login);
 
@@ -23,17 +73,17 @@ public class login extends AppCompatActivity {
             public void onClick(View view) {
                 EditText username = (EditText) findViewById(R.id.username);
                 EditText password = (EditText) findViewById(R.id.password);
-                TextView status = (TextView) findViewById(R.id.status);
 
                 Intent nextScreen = new Intent(getApplicationContext(), Account.class);
-                String user = username.getText().toString();
+                String email = username.getText().toString();
                 String pass = password.getText().toString();
-                if (user.equals("bobwaters") && pass.equals("Cs2340")) {
-                    startActivity(nextScreen);
-                } else {
-                    status.setText("Incorrect username or password");
-                }
+
+                attemptLogin(email, pass);
+
             }
         });
     }
+
+    //TODO write an onStart to check if the user already logged in
+
 }
