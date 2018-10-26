f939c829b7e496d62d4540f22e0c497ea22fe66d
RayTheRunner <raymondbryant98@gmail.com>
2018-10-26 19:16:07 -0400

    Added Skeleton UI for searching

---
+++ app/src/main/java/com/example/sotw/donationtracker/controllers/UserActivity.java
@@ -0,0 +1,45 @@
+package com.example.sotw.donationtracker.controllers;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.sotw.donationtracker.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+
+        Button logout = (Button) findViewById(R.id.user_logout);
+        Button search = (Button) findViewById(R.id.searchButton);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //sign out:
+                FirebaseAuth.getInstance().signOut();
+
+                //switch activities
+                Intent home = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(home);
+            }
+        });
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //switch to search activity
+                Intent searchAct = new Intent(getApplicationContext(), SearchActivity.class);
+                startActivity(searchAct);
+
+            }
+        });
+    }
+}
